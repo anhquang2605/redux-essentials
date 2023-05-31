bb8fd23d4e31f6d9cd963de4443ec65d22784ea0
Anh Quang Chu <anhquang2605@gmail.com>
2023-05-31 10:06:39 -0700

    some changes to postSlice.js

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,6 +1,10 @@
 import {createSlice, nanoid, createAsyncThunk, createSelector} from '@reduxjs/toolkit'
 import {client} from '../../api/client'
 import {sub} from 'date-fns'
+/* const initialState = [
+    {id: '1', title: 'First Post!', content: 'Hello!', date: sub(new Date(), {minutes: 10}).toISOString(), reactions: {thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0}},
+    {id: '2', title: 'Second Post', content: 'More text', date: sub(new Date(), {minutes: 5}).toISOString(), reactions: {thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0}}
+] */
 const initialState = {
     posts: [],
     status: 'idle',
